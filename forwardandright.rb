f2e23ee63229eb7afccd539c66dc10de0f00836c
Raul Cabrera <snappertac@hotmail.com>
2014-02-03 18:00:28 -0700

    Ruby Programming Files
    Two files included are for the movement of the vehicle. one with
    forward and right turn movement, and the other with reverse and left
    turn movement.

---
+++ forwardandright.rb
@@ -0,0 +1,14 @@
+# Forward and Right Turn
+
+A1 = nil
+A2 = nil
+B1 = nil
+B2 = nil
+
+if (A1 == 0 && A2 == 0)
+		# move_forward
+    puts "moving forward"
+	else
+		# turn_right
+    puts "turning right"
+	end
---
+++ reverseandleft.rb
@@ -0,0 +1,14 @@
+# Forward and Right Turn
+
+A1 = nil
+A2 = nil
+B1 = nil
+B2 = nil
+
+if (A1 == 0 && B1 == 1)
+		# reverse
+    puts "reverse"
+	else
+		# turn_left
+    puts "turn left"
+	end
