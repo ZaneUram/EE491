f59e2460a5427c32154f2a14bf6f8f4d3e414a27
bruzoo <bruzoo5@comcast.net>
2014-02-26 19:51:15 -0700

    pwm revision 3
    Changed pin number to differentiate left and right turns.  Also added a
    check for delay

---
+++ pwm_rev3.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/ruby
+frequency = 100
+
+# pwm.rb
+
+def Setup 
+pin_mode = 13,OUTPUT
+end
+
+def Left_Timing_loop (Duty_Cycle)
+digital_writer = 13,HIGH
+delay_microseconds = frequency*Duty_Cycle
+digital_write = 13,LOW
+delay_microseconds = freqency-delay_microseconds
+end
+
+def Right_Timing_loop (Duty_Cycle)
+digital_writer = 12,HIGH
+delay_microseconds = frequency*Duty_Cycle
+digital_write = 12,LOW
+delay_microseconds = freqency-delay_microseconds
+end
+
+def check
+  pseudocount = 5
+  while pseudocount > 4
+    delay_seconds = cycle_time
+  end
+end
+
+  
+
+
+
+
+
